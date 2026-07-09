96b5ab12ec013de04a712d43109db9700aa4955b
robotpay1 <robot.paymeback1@gmail.com>
2026-07-09 00:08:01 +0000

    fix: update base path and entry point script
    Adjust base path to support deployment on specific subdirectories and correct script source path for root resolution.

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import {defineConfig} from 'vite';
 
 export default defineConfig(() => {
   return {
-    base: "./",
+    base: "/Cosmotic-pro-/",
     plugins: [react(), tailwindcss()],
     resolve: {
       alias: {
